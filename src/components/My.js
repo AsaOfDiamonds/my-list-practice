5a31296f3ca4c9755c9b6c867c9d2274d3e04535
AsaOfDiamonds <asaofdiamonds@gmail.com>
2019-09-02 16:15:28 -0600

    scaffolding for My Component

---
+++ src/components/My.js
@@ -0,0 +1,11 @@
+import React from "react";
+
+const My = props => {
+  return(
+    <div className='my'>
+      <h2>this is the My</h2>
+    </div>
+  );
+}
+
+export default My;
--- src/components/MyList.js
+++ src/components/MyList.js
@@ -1,9 +1,11 @@
 import React from "react";
+import My from "./My";
 
 const MyList = props => {
   return(
     <div className='my-list'>
     <h1>This is my List Component</h1>
+      <My />
     </div>
   );
 }
