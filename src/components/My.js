c48770033506da287508fe12379127d88b3a92dc
AsaOfDiamonds <asaofdiamonds@gmail.com>
2019-09-03 20:02:03 -0600

    finished passing state as props to dispaly on MY component

--- src/components/My.js
+++ src/components/My.js
@@ -3,7 +3,8 @@ import React from "react";
 const My = props => {
   return(
     <div className='my'>
-      <h2>this is the My</h2>
+      <h2>{props.item.name}</h2>
+      <p>{props.item.id}</p>
     </div>
   );
 }
