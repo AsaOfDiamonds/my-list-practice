02d8ee12acc8d5404f17a2c8858ee86a228ee408
AsaOfDiamonds <asaofdiamonds@gmail.com>
2019-09-05 11:07:40 -0600

    added state and handleChanges

--- src/components/MyForm.js
+++ src/components/MyForm.js
@@ -7,10 +7,23 @@ class MyForm extends React.Component {
       item: ''
     };
   }
+
+  handleChanges = event => {
+    this.setState({ [event.target.name]: event.target.value })
+  };
+
   render() {
     return(
       <div className= 'my-form'>
         <h1>This is the form</h1>
+        <form>
+          <input 
+            type='text'
+            name= 'item'
+            value={this.state.item}
+            onChange={this.handleChanges}
+          />
+        </form>
       </div>
     );
   }
